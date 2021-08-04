484dbe1435172a6698033c95741528dd388f9f9a
FlawlessAlias <38224637+FlawlessGithub@users.noreply.github.com>
2021-08-04 18:23:14 +0200

    Added history reading
    Version 0.2
    
    The extension now reads the latest URL you visited and prints it in the console! I also learned what a callback in JS is.

--- background.js
+++ background.js
@@ -1,1 +1,10 @@
-console.log("My power grows. I have written in the console.");
+console.log("I have learned basic reading comprehension. Observe as I read you the latest entry from your search history: \n");
+
+// Search history chronologically w/o a filter, but only take 1 entry. Callback function parses raw array.
+chrome.history.search({
+	text: '', 
+	maxResults: 1
+}, function(data){
+	console.log(data[0].url);
+});
+
