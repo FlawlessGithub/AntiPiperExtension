b5b175da3dc882ccd38c13cbc3b6e1cdb8084035
FlawlessAlias <38224637+FlawlessGithub@users.noreply.github.com>
2021-08-06 15:34:52 +0200

    URL-specific removal implemented
    When loaded, the extension will now read the latest entry in they browser history, check it against a list of blocked URLs, and if the latest entry is in the list, remove it from the browser history.

--- background.js
+++ background.js
@@ -1,10 +1,45 @@
-console.log("I have learned basic reading comprehension. Observe as I read you the latest entry from your search history: \n");
+try {
+var urlList = ["https://www.y8.com/", "https://www.newgrounds.com/"];
+
+console.log("Welcome to the console! Don't fuck it up.\n\n");
 
 // Search history chronologically w/o a filter, but only take 1 entry. Callback function parses raw array.
+
+function checkUrl(url) {
+	for (let blockedUrl of urlList){
+		if (url == blockedUrl) {
+			console.log("\nGiven URL: " + url + " matched with blocked URL: " + blockedUrl +"\n\n");
+			console.log();
+			return true;
+			break;
+		}
+		else {
+			console.log("Given URL: " + url + " did not match blocked URL: " + blockedUrl);
+		}
+}
+		console.log("\nGiven URL: " + url + " did not match any blocked URLs:");
+		console.log(urlList);
+		console.log(" ") // Dead space for improved console readability
+}
 chrome.history.search({
 	text: '', 
 	maxResults: 1
-}, function(data){
-	console.log(data[0].url);
-});
+	}, function(historyEntry) {
+		console.log("Latest website in browsing history fetched:");
+		console.log(historyEntry[0].url + "\n\n"); // Dead space included for improved console readability.
+		//console.log(" "); 
+		
+		if (checkUrl(historyEntry[0].url) == true) {
+			chrome.history.deleteUrl({"url": historyEntry[0].url});
+			console.log(historyEntry[0].url + " and all identical URLs have been deleted from the browser history.");
+		}
+	});
+			
+	//checkUrl(latestEntry[0].url);
 
+
+
+	
+} catch (error) {
+console.error(error);
+}
