0594ddfc52780e776d05cb05f823ad7289fbd62b
FlawlessGithub <david.carlstedtringius@gmail.com>
2021-09-07 00:07:44 +0200

    Made popup.js functional w/ Promises
    The commits have been irregular, but I got the popup working perfectly with what I hope is proper syntax using promises!
    
    Now all that's left to do is to overhaul the backend and make some icons.

--- popup.js
+++ popup.js
@@ -1,142 +1,96 @@
-async function getCurrentTabDomain() {
+async function getBlockList() { // Returns a promise containing the blockList array.
+    return new Promise((resolve, reject) => { // Stealing & modifying Google's code just works.
+        // Google: Asynchronously fetch all data from storage.sync.
+        chrome.storage.sync.get(["blockList"], (items) => {
+            // Google: Pass any observed errors down the promise chain.
+            if (chrome.runtime.lastError) {
+                return reject("Google's own code broke in Chrome lmao check getBlockList(): " + chrome.runtime.lastError);
+            }
+
+            items = items["blockList"]; // Is there a better way to do this?
+            if (Array.isArray(items) != true) { // If you fetched literally fucking nothing, make it an empty list, bonehead.
+                items = [];
+            }
+            resolve(items); // This is basically a return but for promises.
+        });
+    });
+}
+
+async function logBlockList() { // Just here so I can check blockList directly from Chrome DevTools easily.
+    console.log(await getBlockList());
+}
+
+function addToBlockList(blockList, domain) { // I don't know what is happening. I had a million glitches yesterday, now it's fine. 
+    console.log(domain + " not in blockList. Pushing...");
+    blockList.push(domain);
+    chrome.storage.sync.set({
+        blockList: blockList
+    }, logBlockList);
+}
+
+function removeFromBlockList(blockList, domain) { // I don't know what is happening. I had a million glitches yesterday, now it's fine. 
+    let index = blockList.indexOf(domain);
+    console.log("Item in blockList. Splicing...");
+    blockList.splice(index, 1);
+    chrome.storage.sync.set({
+        blockList: blockList
+    });
+}
+
+async function getCurrentTabDomain() { // Stealing & modifying Google's code just works.
     let queryOptions = {
         active: true,
         currentWindow: true
     };
     let [tab] = await chrome.tabs.query(queryOptions);
-	return extractDomain(tab.url);
-}
-
-function updateBlockList(newBlockList) {
-    chrome.storage.sync.set({
-            "blockList": newBlockList
-        }, function blockListUpdateHelper() {
-            console.log("blockList updated!");
-        }
-
-    );
+    let bananaSplit = tab.url.split("/");
+    return bananaSplit[0] + "//" + bananaSplit[2]; // Removes paths in URL by splitting at slashes, then concatenating strings.
 }
 
 
-function extractDomain(urlToConvert) {
-    console.log(urlToConvert);
-    // Removes paths in URL by splitting at slashes, then concatenating strings.
-    let bananaSplit = urlToConvert.split("/"); // Reason for the variable here is the mildly amusing name.
-    //return bananaSplit[0] + "//" + bananaSplit[2]; // Replace with callback function???
-    console.log(bananaSplit[0] + "//" + bananaSplit[2]);
-    return bananaSplit[0] + "//" + bananaSplit[2];
-}
-
-function toggle(button, root, domain, blockedDomains) {
-  
-
-  if (button.innerHTML == "ENABLED") {
-        button.innerHTML = "DISABLED";
-		root.style.setProperty('--button-color', 'var(--red)');
-        // blockList shenanignans
-        
-		index = blockedDomains.indexOf(domain);
-        if (index !== -1) {	// indexOf returns -1 if it's not in the array. If you try to splice -1, it removes the second last item in the array.
-            console.log(blockedDomains.splice(index, 1) + " removed from the blockList.");
-            chrome.storage.sync.set({
-                "blockList": blockedDomains
-            });
-            console.log(chrome.storage.sync.get(["blockList"]));
-
-
-        } else {
-            console.log(domain + " isn't in the blockList! No changes made.");
-        }
-
-    } else {
-        button.innerHTML = "ENABLED";
+function startUp(blockList, domain) { // Set initial button state.	
+    console.log("startUp run with the following values: " + blockList + " and " + domain);
+    let btn = document.getElementById("btn");
+    let root = document.documentElement;
+    if (blockList.includes(domain)) {
+        btn.innerHTML = "ENABLED";
         root.style.setProperty('--button-color', 'var(--green)');
-
-        // blockList shenanigans
-        if (blockedDomains.indexOf(domain) == -1) { // indexOf returns -1 if it's not in the array.
-            chrome.storage.sync.set({
-                "blockList": blockedDomains.push(domain)
-            });
-
-            console.log(domain + " added to the blockList.");
-            console.log(blockedDomains);
-        } else {
-            console.log(domain + " is already in the blockList!");
-        }
-    }
-}
-
-async function refreshBlockList(callback) { // Umbrella function. Gets blockList from online, sets initial button state based off domain.
-    let blockList = await new Promise((resolve, reject) => {
-    // Asynchronously fetch all data from storage.sync.
-    chrome.storage.sync.get(["blockList"], (items) => {
-      // Pass any observed errors down the promise chain.
-      if (chrome.runtime.lastError) {
-        return reject(chrome.runtime.lastError);
-      }
-      // Pass the data retrieved from storage down the promise chain.
-	console.log("items");
-	console.log(items);
-	resolve(items);
-	});
-	});
-	if (!Array.isArray(blockList)) blockList = [];	// Hack solution. Fix later.
-	console.log("blockList");
-	console.log(blockList);
-    return blockList;
-}
-
-/*
-function addDomainToBlockList(domain, blockList) {
-    if (blockList.indexOf(domain) == -1) { // indexOf returns -1 if it's not in the array.
-        chrome.storage.sync.set({
-            "blockList": blockList.push(domain)
-        });
-        console.log(domain + " added to the blockList.");
     } else {
-        console.log(domain + " is already in the blockList!");
+        btn.innerHTML = "DISABLED";
+        root.style.setProperty('--button-color', 'var(--red)');
     }
+    document.getElementById("currentDomain").innerHTML = domain;
 }
 
-function removeDomainFromBlockList(domain, blockList) {
-    index = blockList.indexOf(domain);
-    if (index !== -1) { // // indexOf returns -1 if it's not in the array. If you try to splice -1, it removes the second last item in the array.
-        console.log(blockList.splice(index, 1) + " removed from the blockList.");
-        chrome.storage.sync.set({
-            "blockList": blockList
-        });
-    } else {
-        console.log(domain + " isn't in the blockList! No changes made.");
-    }
-}
-*/
 
 try {
-    var root = document.documentElement; // For CSS variable editing.
-    var currentDomain = getCurrentTabDomain();
-	var blockList = refreshBlockList();
-	var blockBtn = document.getElementById("btn");
+    Promise.all([getBlockList(), getCurrentTabDomain()]).then( // Combine results from two promises into array. Index array for individual results.
+        values => startUp(values[0], values[1])
+    );
 
-	document.getElementById("currentDomain").innerHTML = currentDomain;
-		
-	console.log("Current domain: ");
-	console.log(currentDomain);
-	console.log("Blocked URLs: ");
-	console.log(blockList);
-	console.log("Blocked URLs length: " + blockList.length);
-            
-    if (blockList.includes(currentDomain)) {
-        blockBtn.innerHTML = "ENABLED";
-        root.style.setProperty('--button-color', 'var(--green)');
-    } else {
-        blockBtn.innerHTML = "DISABLED";
-        root.style.setProperty('--button-color', 'var(--red)');
-        }
-		
-    blockBtn.addEventListener("click", function toggler() {
-		toggle(blockBtn, root, currentDomain, blockList);
-			});
+    let btn = document.getElementById("btn");
+    let root = document.documentElement;
 
+    btn.addEventListener("click", function toggleButton() {
+
+        Promise.all([getBlockList(), getCurrentTabDomain()]).then(values => {
+
+            let blockList = values[0];
+            let domain = values[1];
+
+            if (btn.innerHTML == "ENABLED") {
+                btn.innerHTML = "DISABLED";
+                root.style.setProperty('--button-color', 'var(--red)');
+                removeFromBlockList(blockList, domain);
+
+            } else {
+                btn.innerHTML = "ENABLED";
+                root.style.setProperty('--button-color', 'var(--green)');
+                addToBlockList(blockList, domain);
+
+            }
+        });
+    })
 } catch (error) {
-    console.error(error);
+    console.log(error);
 }
